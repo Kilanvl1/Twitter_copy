b670f8a8e4f83cb8160a73200b3fa640468528db
Kilan_vanloo <27832708@sun.ac.za>
2023-08-07 09:19:45 +0200

    Pushed starting version up to github

---
+++ src/index.js
@@ -0,0 +1,78 @@
+const express = require('express'); 
+const fs = require('fs');
+
+require('./db/mongoose');
+
+const Note = require('./models/note')
+
+const app = express(); 
+
+app.use(express.json())
+
+//CRUD
+
+//CREATE
+app.post('/notes', async (req, res) => {
+    const note = new Note(req.body)
+
+    try {
+        await note.save()
+        res.status(201).send(note)
+    } 
+    catch (err) {   
+        res.status(400).send(err)
+    }
+})
+
+//READ
+app.get('/notes', async (req, res) => {
+
+    try {
+        const notes = await Note.find({})
+        res.send(notes)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+//UPDATE
+
+app.patch('/notes/:id', async (req, res) => {
+    try {
+        const note = await Note.findById(req.params.id) 
+
+        if(!note) {
+            return res.status(404).send()
+        }
+    
+        note.note = req.body.note 
+
+        await note.save()
+
+        res.status(300).send(note)
+    } 
+    catch (err) {
+        res.status(404).send(err)
+    }
+
+})
+
+//DElETE
+app.delete("/notes/:id", async (req, res) => {
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id)
+
+        if(!note) {
+            return res.status(404).send()
+        }
+
+        res.send("The note has been deleted")
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+app.listen(3000, () => {
+    console.log("The server is up on port 3000")
+})
